6f9e721f0f00f3ec14868b54bb5f62ab524387e8
mkdir01 <projectdevelopinginbox@gmail.com>
2021-06-01 18:57:06 -0500

    text and color changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <div className="header-info">
-                <h1 className="app-title">Name</h1>
+                <h1 className="app-title">Mkdir</h1>
                 <p className="project-source">
                     <a
                         target="_blank"
