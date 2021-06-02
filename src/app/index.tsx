6a782c56a0bf6db769c3be9f28d1bdf57e5767d0
mkdir01 <projectdevelopinginbox@gmail.com>
2021-06-01 19:04:28 -0500

    Add Avatar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 import Canvas from 'app/components/canvas';
+import mkdiravatar from'./mkdir-avatar.png';
 
 function App() {
     return (
         <div className="App">
             <div className="header-info">
+				<img src={mkdiravatar} width="177px" height="177px"/>
                 <h1 className="app-title">Mkdir</h1>
                 <p className="project-source">
                     <a
                         target="_blank"
                         rel="noreferrer"
-                        href="https://github.com/mkdir01"
+                        href="https://github.com/mkdir01/site"
                     >
                         [ Find project on GitHub ]
                     </a>
